dec01c0b599c56fc7767d8511a986ea29be941cb
Dmitry Gluzdov <Dmitry@Gluzdov.ru>
2023-05-21 14:25:06 +0300

    Config refactored. Now after import "from src.config import *" you can use var CONFIG for common parameters.
    Config for Flask was renamed: FlaskConfiguration

--- src/config.py
+++ src/config.py
@@ -1,5 +1,28 @@
-class Configuraton(object):
+import os
+import logging
+
+
+class FlaskConfiguration(object):
     DEBUG = True
 
 
-DB_FULL_PATH = ""
+class CommonConfiguration(object):
+    DEBUG = True
+    DB_FULL_PATH = ""
+
+    def __init__(self):
+        if self.DEBUG:
+            debug_logs_path = os.path.join(os.path.abspath(os.curdir), "debug.logs")
+
+            if not os.path.isdir(debug_logs_path):
+                os.mkdir(debug_logs_path)
+
+            log_file_name = os.path.join(debug_logs_path, "debug.log")
+            logging.basicConfig(filename=log_file_name, level=logging.DEBUG)
+
+
+CONFIG = CommonConfiguration()
+
+__ALL__ = [
+    "CONFIG",
+ ]
