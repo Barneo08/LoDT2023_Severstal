212dfb7f4fb946d87a55b012e97b34800cb92b3a
Dmitry Gluzdov <Dmitry@Gluzdov.ru>
2023-05-24 00:06:43 +0300

    The raw data loading procedure includes the x_train and y_train datasets.

--- load_raw_data.py
+++ load_raw_data.py
@@ -9,6 +9,16 @@ import src.utils as utils
 
 def load_data(sql_server_type):
     module_name = "Load raw data to DB"
+
+    if not CONFIG.RAW_FILES_UPLOAD_ENABLED:
+        print("-------------------------------------------------------------------")
+        utils.log_print("Uploading raw files is blocked in the settings.", module_name=module_name)
+        print("-------------------------------------------------------------------")
+        print()
+        print("Terminated.")
+        return
+
+    return
     utils.log_print(f"Starting loading raw data to DBase ({sql_server_type})...", module_name=module_name)
     if not utils.if_all_modules_exist(constants.MODULES_PARAM_GROUPS["initdb"]):
         utils.log_print("One of the modules is missing. The program is terminated.", module_name="Load raw data to DB")
@@ -17,49 +27,56 @@ def load_data(sql_server_type):
         import src.db as db
         dh = db.DataHandler(sql_server_type)
 
-        # Получаем информация о загружаемой таблице
-        raw_table_name = "Y_TRAIN_RAW"
-        file_path = os.path.join(os.path.abspath(os.curdir), CONFIG.RAW_DATA_FULL_PATH, "y_train.parquet")
-        df = pd.read_parquet(file_path, engine="fastparquet")
-        # df = df.head(99)
+        conf = {
+            "X_TRAIN_RAW": {"raw_data_file": "x_train.parquet", "columns_subs": constants.SENSOR_FIELD_NAMES_LIST},
+            "Y_TRAIN_RAW": {"raw_data_file": "y_train.parquet", "columns_subs": constants.Y_LIST},
+        }
 
-        # Заменим названия столбцов на удобные:
-        df.rename(columns=constants.Y_LIST, inplace=True)
+        for raw_table_name in conf.keys():
+            # Получаем информация о загружаемой таблице
+            # raw_table_name = "Y_TRAIN_RAW"
+            file_path = os.path.join(os.path.abspath(os.curdir), CONFIG.RAW_DATA_FULL_PATH, conf[raw_table_name]["raw_data_file"])
+            df = pd.read_parquet(file_path, engine="fastparquet")
+            # df = df.head(99)
 
-        # Создаём таймер
-        timer = utils.Timer("Общий таймер. Загрузка сырых данных.", only_on_show=False)
+            # Заменим названия столбцов на удобные:
+            df.rename(columns=conf[raw_table_name]["columns_subs"], inplace=True)
 
-        rows = df.shape[0]
-        print(f"{raw_table_name}:")
-        print(f"Количество строк для загрузки: {utils.sep_digits(rows)} шт.")
-        print(f"Загрузка осуществляется блоками по {utils.sep_digits(CONFIG.UPLOAD_ROWS)} строк.")
+            # Создаём таймер
+            timer = utils.Timer("Общий таймер. Загрузка сырых данных.", only_on_show=False)
 
-        for element in constants.E_LIST:
-            # Получим префикс (имя) для эксгаустера
-            e_name = element + constants.EXH_SEPARATOR
-            # Отберём колонки в данных, которые начинаются с этого имени
-            y_columns_list = sorted([one_column for one_column in df.columns if one_column[:len(e_name)] == e_name])
-            if len(y_columns_list) != 0:
-                # timer_element = utils.Timer(f"Эксгаустер: {element}")
-                df_2_load = df[y_columns_list]
-                # Имя для таблицы:
-                table_name = e_name + raw_table_name
-                # Загружаем данные
-                utils.log_print(f"Загрузка сырых данных y_train для эксгаузера {element} в DBase в таблицу {table_name}.", module_name=module_name, to_screen=False)
+            rows = df.shape[0]
+            print(f"{raw_table_name}:")
+            print(f"Количество строк для загрузки: {utils.sep_digits(rows)} шт.")
+            print(f"Загрузка осуществляется блоками по {utils.sep_digits(CONFIG.UPLOAD_ROWS)} строк.")
 
-                # Если таблица есть, то её надо удалить.
-                dh.execute(f'DROP TABLE IF EXISTS "{table_name}"')
-                dh.commit()
+            for element in constants.E_LIST:
+                # Получим префикс (имя) для эксгаустера
+                e_name = element + constants.EXH_SEPARATOR
+                # Отберём колонки в данных, которые начинаются с этого имени
+                y_columns_list = sorted([one_column for one_column in df.columns if one_column[:len(e_name)] == e_name])
+                y_columns_list = sorted(y_columns_list)
+                if len(y_columns_list) != 0:
+                    # timer_element = utils.Timer(f"Эксгаустер: {element}")
+                    df_2_load = df[y_columns_list]
+                    # Имя для таблицы:
+                    table_name = e_name + raw_table_name
+                    # Загружаем данные
+                    utils.log_print(f"Загрузка сырых данных y_train для эксгаузера {element} в DBase в таблицу {table_name}.", module_name=module_name, to_screen=False)
 
-                # Загрузим данные частями по UPLOAD_ROWS
-                def data_2_sql(num1, num2):
-                    df_2_load.iloc[num1: num2].to_sql(table_name, con=dh.connector, if_exists="append")
+                    # Если таблица есть, то её надо удалить.
+                    dh.execute(f'DROP TABLE IF EXISTS "{table_name}"')
+                    dh.commit()
 
-                for num in tqdm(range(int(rows / CONFIG.UPLOAD_ROWS) + 1), ncols=75, desc=f"Эксгаузер: {element}"):
-                    data_2_sql(num * CONFIG.UPLOAD_ROWS, (num * CONFIG.UPLOAD_ROWS) + CONFIG.UPLOAD_ROWS)
+                    # Загрузим данные частями по UPLOAD_ROWS
+                    def data_2_sql(num1, num2):
+                        df_2_load.iloc[num1: num2].to_sql(table_name, con=dh.connector, if_exists="append")
 
-        # Выведем информацию о времени исполнения блока кода:
-        timer.show()
+                    for num in tqdm(range(int(rows / CONFIG.UPLOAD_ROWS) + 1), ncols=75, desc=f"Эксгаузер: {element}"):
+                        data_2_sql(num * CONFIG.UPLOAD_ROWS, (num * CONFIG.UPLOAD_ROWS) + CONFIG.UPLOAD_ROWS)
+
+            # Выведем информацию о времени исполнения блока кода:
+            timer.show()
 
 
 if len(sys.argv) == 1:
@@ -75,6 +92,7 @@ if len(sys.argv) == 1:
     print("--------------------------------------------")
     pressed_key = input().upper().replace(" ", "")
     if pressed_key == "Y":
+        print()
         load_data("SQLite")
     else:
         print("Terminated.")
@@ -104,6 +122,7 @@ else:
 
                 pressed_key = input().upper().replace(" ", "")
                 if pressed_key == "Y":
+                    print()
                     load_data(param_value)
                 else:
                     print("Terminated.")
