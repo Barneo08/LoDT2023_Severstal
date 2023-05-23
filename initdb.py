7af64f26365531760246cc99218fa54e577f25fb
Dmitry Gluzdov <Dmitry@Gluzdov.ru>
2023-05-23 10:32:43 +0300

    Added new util

---
+++ initdb.py
@@ -0,0 +1,12 @@
+import src.constants as constants
+import src.utils as utils
+
+
+# запускается блок по созданию и инициализации базы данных:
+module_name = "Init DB"
+utils.log_print("Starting initialisation of DBase...", module_name=module_name)
+if utils.if_all_modules_exist(constants.MODULES_PARAM_GROUPS["initdb"]):
+    import src.db as db
+    db.DataHandler()
+else:
+    utils.log_print("One of the modules is missing. The program is terminated.", module_name=module_name)
