850b2c7ea28f21c7b6cfbdf228eb2b9671a3ced3
Dmitry Gluzdov <Dmitry@Gluzdov.ru>
2023-05-24 00:09:14 +0300

    Created the ability to use your local config stored in the _local_config.py file. Its presence leads to the substitution of the CommonConfiguration class.

--- src/config.py
+++ src/config.py
@@ -6,27 +6,31 @@ class FlaskConfiguration(object):
     DEBUG = True
 
 
-class CommonConfiguration(object):
-    DEBUG = True
-    DB_FULL_PATH = "DBase"
-    RAW_DATA_FULL_PATH = "indata"
+if os.path.isfile("_local_config.py"):
+    from _local_config import *
+else:
+    class CommonConfiguration(object):
+        DEBUG = True
+        DB_FULL_PATH = "DBase"
+        RAW_DATA_FULL_PATH = "indata"
 
-    SQL_SERVER_TYPE = "PostgreSQL"  # (SQLite, PostgreSQL)
-    SQL_SERVER_HOST = "localhost"
-    SQL_SERVER_USERNAME = "postgres"
-    SQL_SERVER_PASSWORD = "123"
-    SQL_SERVER_DB_NAME = "SeverStal_DB"
-    UPLOAD_ROWS = 1_000_000
+        SQL_SERVER_TYPE = "PostgreSQL"  # (SQLite, PostgreSQL)
+        SQL_SERVER_HOST = "localhost"
+        SQL_SERVER_USERNAME = "postgres"
+        SQL_SERVER_PASSWORD = "123"
+        SQL_SERVER_DB_NAME = "SeverStal_DB"
+        UPLOAD_ROWS = 1_000_000
+        RAW_FILES_UPLOAD_ENABLED = True
 
-    def __init__(self):
-        if self.DEBUG:
-            debug_logs_path = os.path.join(os.path.abspath(os.curdir), "debug.logs")
+        def __init__(self):
+            if self.DEBUG:
+                debug_logs_path = os.path.join(os.path.abspath(os.curdir), "debug.logs")
 
-            if not os.path.isdir(debug_logs_path):
-                os.mkdir(debug_logs_path)
+                if not os.path.isdir(debug_logs_path):
+                    os.mkdir(debug_logs_path)
 
-            log_file_name = os.path.join(debug_logs_path, "debug.log")
-            logging.basicConfig(filename=log_file_name, level=logging.DEBUG)
+                log_file_name = os.path.join(debug_logs_path, "debug.log")
+                logging.basicConfig(filename=log_file_name, level=logging.DEBUG)
 
 
 CONFIG = CommonConfiguration()
