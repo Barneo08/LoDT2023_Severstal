9ae35a5f04bf31e6c6a1dd672fd905d093027fba
Dmitry Gluzdov <Dmitry@Gluzdov.ru>
2023-05-27 21:08:44 +0300

    Updated procedure of data prediction.

--- src/prediction/__init__.py
+++ src/prediction/__init__.py
@@ -1,7 +1,10 @@
-import datetime
+import numpy as np
+import os.path
+
 import pandas as pd
-from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LogisticRegression
 from tqdm import tqdm
+import joblib
 import warnings
 
 import src.constants as constants
@@ -10,78 +13,6 @@ import src.utils as utils
 
 
 warnings.filterwarnings('ignore')
-
-
-def make_prediction():
-    """
-        Do prediction for each stock
-    """
-    otimer = utils.Timer("Get started process of stocs's prices prediction:")
-    dh = db.DataHandler(keep_silence=False)
-    stocks_list = dh.get_stocks_list()
-    for one_stoks in stocks_list:
-        print("\nPredicting prices for {}, {}".format(one_stoks[0], one_stoks[2]))
-        make_stock_prediction(mfd_id=one_stoks[1], db_connection=dh)
-    otimer.show()
-
-
-def make_stock_prediction(mfd_id, db_connection):
-    """
-        Make pridiction for 4 prices.
-    """
-    X_tmp = make_stock_prediction_for_one_price(db_connection, mfd_id=mfd_id, sprice_type="OPEN")
-
-    X_predicted = X_tmp[["y"]].copy()
-    X_predicted["price_open"] = X_predicted["y"]
-    X_predicted["vol"] = 0
-    X_predicted["ppredict"] = 1
-
-    X_tmp = make_stock_prediction_for_one_price(db_connection, mfd_id=mfd_id, sprice_type="CLOSE")
-    X_predicted["price_close"] = X_tmp["y"]
-
-    X_tmp = make_stock_prediction_for_one_price(db_connection, mfd_id=mfd_id, sprice_type="MIN")
-    X_predicted["price_min"] = X_tmp["y"]
-
-    X_tmp = make_stock_prediction_for_one_price(db_connection, mfd_id=mfd_id, sprice_type="MAX")
-    X_predicted["price_max"] = X_tmp["y"]
-
-    X_predicted = X_predicted.reset_index()
-    X_predicted = X_predicted[["price_dt", "price_open", "price_min", "price_max", "price_close", "vol", "ppredict"]]
-
-    db_connection.load_stock_prises_from_df2db(mfd_id=mfd_id, id_period_type=constants.DEFAULT_PERIOD_TYPE, df=X_predicted)
-
-
-def make_stock_prediction_for_one_price(db_connection, mfd_id, sprice_type):
-    """
-        Do prediction for one element - one type of price.
-    """
-    print("Формируем предсказание для цены: ", sprice_type)
-    df = db_connection.get_stocks_prices_pd(
-                    mfd_id=mfd_id,
-                    id_period_type=constants.DEFAULT_PERIOD_TYPE,
-                    price_type=sprice_type,
-                    dt_begin=constants.FIRST_DAY_OF_PREDICTION_DATASET,
-                    ppredict=0
-                                            )
-    df["price_dt"] = df["price_dt"].apply(lambda x: datetime.datetime.strptime(x, constants.DATETIME_FORMAT_DASH))
-    # Максимальная дата - дата последней строки:
-    last_datetime = df.price_dt.max()
-    df_last_row = df.shape[0]
-    # Подготовим строчки для предсказания, добавив их в основную таблицу - 
-    # тут они правильно заполнятся:
-    for j in range(constants.DEPTH_OF_PREDICTION):
-        last_datetime = last_datetime + constants.PREDICTION_STEP
-        if last_datetime.hour>=20:
-            last_datetime = last_datetime + datetime.timedelta(hours=(9+24-last_datetime.hour))
-
-        new_row = pd.DataFrame([{"price": 0, "price_dt": last_datetime, "vol": 0}])
-        new_row.index = [df.shape[0]]
-        df = df.append(new_row)
-
-    X_train, X_test = prepare_dataset(df, df_last_row)
-
-    X_test = prepare_prediction(X_train, X_test)
-    return X_test
 
 
 def prepare_prediction(X, X_test):
@@ -106,13 +37,13 @@ def prepare_prediction(X, X_test):
         # А первый лаг - это целевой признак:
         X_row4predict["lag_1"] = X_last_row["y"].values[0]
         # Обучим:
-        lr = LinearRegression()
+        lr = LogisticRegression()
         lr.fit(X_train, y_train)
         # Сделаем предсказание, убрав столбец с целевым признаком:
         prediction = lr.predict(X_row4predict.drop(["y"], axis=1))
         X_row4predict["y"] = prediction[0]
 
-        # Добавим строку с предсказанием в обучающйю выборку для дальнейшего использования:
+        # Добавим строку с предсказанием в обучающую выборку для дальнейшего использования:
         # X_row4predict.index = [X.shape[0]]
         X = X.append(X_row4predict)
         X["y"] = X["y"]
@@ -120,41 +51,138 @@ def prepare_prediction(X, X_test):
     return X[ndivider:]
 
 
-def prepare_dataset(df, ndivider):
-    lag_end = constants.MAX_LAG_FOR_PREDICTION
+def make_prediction_for_one_teh_place(X, teh_place_id):
+    model_path = utils.get_get_model_full_file_path(teh_place_id, "M1")
+    if os.path.isfile(model_path):
+        model = joblib.load(model_path)
+        prediction1 = model.predict_proba(X)[:, 1]
+    else:
+        prediction1 = None
 
-    dataset = df.copy()
-    dataset = dataset.set_index("price_dt")
-    dataset = dataset[["price"]]
-    dataset.columns = ["y"]
+    model_path = utils.get_get_model_full_file_path(teh_place_id, "M3")
+    if os.path.isfile(model_path):
+        model = joblib.load(model_path)
+        prediction3 = model.predict_proba(X)[:, 1]
+    else:
+        prediction3 = None
 
-    # добавляем лаги исходного ряда в качестве признаков
-    for j in range(1, lag_end + 1):
-        dataset["lag_{}".format(j)] = dataset.y.shift(j)
+    if prediction1 is None and prediction3 is None:
+        return None
+    if prediction1 is not None and prediction3 is None:
+        return (prediction1 >= 0.5) * 1
+    if prediction1 is None and prediction3 is not None:
+        return (prediction3 >= 0.5) * 1
 
-    dataset["month"] = dataset.index.month
-    dataset["day"] = dataset.index.day
-    dataset["hour"] = dataset.index.hour
-    dataset["minute"] = dataset.index.minute
-    dataset["weekday"] = dataset.index.weekday + 1
-    for j in range(1,13):
-        dataset[f"month{j}"] = dataset["month"].apply(lambda x: 1 if x == j else 0)
-    for j in range(1,32):
-        dataset[f"day{j}"] = dataset["day"].apply(lambda x: 1 if x == j else 0)
-    for j in range(1,8):
-        dataset[f"weekday{j}"] = dataset["weekday"].apply(lambda x: 1 if x == j else 0)
-    for j in range(8,21):
-        dataset[f"hour{j}"] = dataset["hour"].apply(lambda x: 1 if x == j else 0)
-    for j in [0,10,20,30,40,50]:
-        dataset[f"minute{j}"] = dataset["minute"].apply(lambda x: 1 if x == j else 0)
+    prediction_ret = (prediction1 >= 0.5) * 1 + ((prediction3 > 0.5) & (prediction1 < 0.5)) * 2
+    # prediction_ret = prediction_ret.astype("int")
 
-    dataset["hour"] = dataset.index.hour
-    dataset['is_weekend'] = dataset.weekday.isin([5,6])*1
-    # выкидываем признаки, созданные для кодирования средних
-    dataset.drop(["month", "day", "weekday", "hour", "minute"], axis=1, inplace=True)
+    return prediction_ret
 
-    X_train = dataset[:ndivider]
-    X_test = dataset[ndivider:]
-    # Удалим строки с образовавшимися при смещении пустыми ячейками:
-    X_train = X_train.dropna()
-    return X_train, X_test
+
+def make_prediction_for_one_exh(exh_id):
+    X, y_full = prepare_dataset(exh_id=exh_id, process_type="test")
+    for one_teh_place in y_full.columns:
+        y_for_one_teh_place = make_prediction_for_one_teh_place(X, teh_place_id=one_teh_place)
+        if y_for_one_teh_place is not None:
+            y_full[one_teh_place] = y_for_one_teh_place
+
+    dh = db.DataHandler("Pandas", keep_silence=True)
+    y_exh_test_file_path = dh.connector.get_file_path(exh_id, type_data="Y", type_for="test")
+    dh.connector.to_file(df=y_full, table_name=y_exh_test_file_path)
+
+
+def make_prediction_for_all_exh():
+    print()
+    utils.log_print("Начато формирование предсказаний по каждому объекту", module_name="make_prediction_for_all_exh", color="b")
+    for one_exh in constants.E_DICT.values():
+        print(f" Эксгаузер: {one_exh}")
+        make_prediction_for_one_exh(one_exh)
+    utils.log_print("Предсказания сформированы", module_name="make_prediction_for_all_exh", color="b")
+    print()
+
+
+def prepare_models_for_one_exh(exh_id):
+    X, y_full = prepare_dataset(exh_id=exh_id, process_type="train")
+    # Первый уровень цикла - это технические места,
+    # которым соответствует отдельный столбец в Y
+    for one_teh_place in constants.Y_LIST.values():
+        if one_teh_place[:len(exh_id)] != exh_id:
+            continue
+        # На всякий случай проверим есть ли такой столбец в списке столбцов в Y
+        if one_teh_place in y_full.columns:
+            # -----------------------------
+            # Формирование модели для M1
+            y_m1 = y_full[[one_teh_place]]
+            y_m1[one_teh_place] = y_m1[one_teh_place].apply(lambda data: 1 if data == 1 else 0)
+            unique_y_m1 = y_m1[one_teh_place].unique()
+            if len(unique_y_m1) == 2:
+                lr_m1 = LogisticRegression()
+                lr_m1.fit(X, y_m1)
+                model_file_name = utils.get_get_model_full_file_path(teh_place_id=one_teh_place, model_type="M1")
+                if os.path.isfile(model_file_name):
+                    os.remove(model_file_name)
+                joblib.dump(lr_m1, model_file_name)
+            # -----------------------------
+            # Формирование модели для M3
+            y_m3 = y_full[[one_teh_place]]
+            y_m3[one_teh_place] = y_m3[one_teh_place].apply(lambda data: 2 if data == 2 else 0)
+            unique_y_m3 = y_m3[one_teh_place].unique()
+            if len(unique_y_m3) == 2:
+                lr_m3 = LogisticRegression()
+                lr_m3.fit(X, y_m3)
+                model_file_name = utils.get_get_model_full_file_path(teh_place_id=one_teh_place, model_type="M3")
+                if os.path.isfile(model_file_name):
+                    os.remove(model_file_name)
+                joblib.dump(lr_m3, model_file_name)
+            # -----------------------------
+
+
+def build_and_save_all_models():
+    # Переберём все эксгаустеры и создадим
+    # для каждого необходимый набор моделей:
+    print()
+    utils.log_print("Начата генерация моделей для всех объектов", module_name="build_and_save_all_models", color="b")
+    for one_exh in constants.E_DICT.values():
+        print(f" Эксгаузер: {one_exh}")
+        prepare_models_for_one_exh(one_exh)
+    utils.log_print("Генерация завершена", module_name="make_prediction_for_all_exh", color="b")
+    print()
+
+
+def prepare_dataset(exh_id, process_type="train"):
+    dh = db.DataHandler("Pandas", keep_silence=True)
+    x_file_path = dh.connector.get_file_path(exh_id, type_data="X", type_for=process_type)
+    y_file_path = dh.connector.get_file_path(exh_id, type_data="Y", type_for=process_type)
+
+    # Загружаем необходимый набор данных.
+    df_x = None
+    df_y = None
+    if os.path.isfile(x_file_path):
+        df_x = pd.read_parquet(x_file_path, engine="fastparquet")
+    if os.path.isfile(y_file_path):
+        df_y = pd.read_parquet(y_file_path, engine="fastparquet")
+
+    if process_type == "train":
+        # Эти данные необходимо привести к одному количеству строк,
+        # так как на момент загрузки по обеим таблицам был "сдвиг"
+        df_merged = df_x.merge(df_y, how="inner", left_index=True, right_index=True)
+        df_x = df_merged[df_x.columns]
+        df_y = df_merged[df_y.columns]
+    elif process_type == "test":
+        # Создадим заготовку:
+        df_y = df_x[[df_x.columns[0]]].copy()
+        df_y.rename(columns={df_x.columns[0]: "TempColumn"}, inplace=True)
+        df_y["TempColumn"] = 0
+
+        # Выберем все технические места для данного exh_id
+        this_exh_all_teh_place = sorted([col for col in constants.Y_LIST.values() if col[:len(exh_id)] == exh_id])
+
+        # В созданной заготовке создадим столбцы для каждого технического с нулями:
+        for one_tp in this_exh_all_teh_place:
+            df_y[one_tp] = df_y["TempColumn"]
+
+        df_y.drop(["TempColumn"], axis=1, inplace=True)
+
+    return df_x, df_y
+
+
